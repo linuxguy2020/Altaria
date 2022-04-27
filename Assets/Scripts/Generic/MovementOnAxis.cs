03165941f32d17efb9551010d6affedbc594f98f
gioiannu <gioiannu97@libero.it>
2022-04-27 23:03:41 +0200

    movement script fix

--- Assets/Scripts/Generic/MovementOnAxis.cs
+++ Assets/Scripts/Generic/MovementOnAxis.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectMovement : MonoBehaviour
+public class MovementOnAxis : MonoBehaviour
 {
 
     public float speed = 0.3f;
@@ -11,13 +11,10 @@ public class ObjectMovement : MonoBehaviour
 
     public bool moving = false;
     public bool falling = false;
-    public GameObject Player;
-
 
 
     private bool positiveDirection = true;
     private Vector3 spawnPos;
-    private bool attached = false;
 
     // Start is called before the first frame update
     void Start()
@@ -97,10 +94,6 @@ public class ObjectMovement : MonoBehaviour
             {
                 // move right
                 transform.Translate(Vector3.right * speed * Time.deltaTime);
-                if (attached == true)
-                {
-                    Player.transform.Translate(Vector3.right * speed * Time.deltaTime);
-                }
                 if (transform.position.x >= spawnPos.x + range)
                 {
                     positiveDirection = false;
@@ -110,10 +103,6 @@ public class ObjectMovement : MonoBehaviour
             {
                 // move left
                 transform.Translate(Vector3.left * speed * Time.deltaTime);
-                if (attached == true)
-                {
-                    Player.transform.Translate(Vector3.left * speed * Time.deltaTime);
-                }
                 if (transform.position.x <= spawnPos.x - range)
                 {
                     positiveDirection = true;
@@ -127,27 +116,4 @@ public class ObjectMovement : MonoBehaviour
         transform.Translate(Vector3.down * 5.8f * Time.deltaTime);
 
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Collisione con:" + other.name);
-        if (other.gameObject == Player)
-        {
-            attached = true;
-            /*Debug.Log("Collisione con player:");
-            lastTransform = Player.transform.parent.transform.parent;
-            Debug.Log("Ultimo transform:" + lastTransform.name);
-            Debug.Log("Nuovo transform:" + transform.name);
-            Player.transform.parent.transform.parent = transform;*/
-        }
-    }
-
-     private void OnTriggerExit(Collider other)
-     {
-         Debug.Log("Uscita dalla collisione con:" + other.name);
-         if (other.gameObject == Player)
-         {
-            attached = false;
-         }
-     }
 }
